429b622b724353f2bb68190c34c52183964e64d8
ehssanmp <59968835+ehssanmp@users.noreply.github.com>
2020-07-19 12:24:23 +0430

    Add files via upload

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@
 package simulator;
 
 import simulator.control.Simulator;
+import simulator.gates.combinational.And;
 import simulator.gates.combinational.Memory;
 import simulator.gates.combinational.Not;
 
@@ -18,24 +19,38 @@ public class Sample {
     public static void main(String[] args) {
     	
     	//sample circuit
-    	Link [] links= new Link[1024];
-    	for(int i=1; i<32;++i) {
-    		links[i]=Simulator.trueLogic;
-    	}
-    	for(int i=32;i<1024;++i) {
-    		links[i]=Simulator.falseLogic;
-    	}
-    	/*Mux mx= new Mux("hello", "37X1",Simulator.trueLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic);
-    	for(int i=0;i<32;++i) {
-    		mx.addInput(links[i]);
-    	}*/
-    	
-    	MultiP mp= new MultiP("hello","1029X32",Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic,Simulator.falseLogic);
-    	for(int i=0;i<1024;++i) {
-    		mp.addInput(links[i]);
-    	}
-    	links[0]=Simulator.falseLogic;
+    	Register [] Reg= new Register[32];
+    	Link[] WriteData= new Link[32];
     	Clock clk = new Clock("CLOCK",1000);
+    	Link RegWrite= Simulator.falseLogic;
+    	Link[] WriteRegisterNumber= new Link[5];
+    	Link[] ReadRegisterNumberA= new Link[5];
+    	Link[] ReadRegisterNumberB= new Link[5]; 
+    	And[] ands= new And[32];
+    	MultiPlexer DEC= new MultiPlexer("DEC","5X32",WriteRegisterNumber[0],WriteRegisterNumber[1],WriteRegisterNumber[2],WriteRegisterNumber[3],WriteRegisterNumber[4]);
+    	for(int i=0;i<32;++i) {
+    		ands[i]= new And("a"+i,DEC.getOutput(i),RegWrite);
+    	}
+    	for(int i=0; i<32;++i) {
+    		Reg[i]= new Register("h"+i, "33X32",ands[i].getOutput(i));
+    		for(int j=0; j<32;++j) {
+    			Reg[i].addInput(WriteData[j]);
+    		}
+    	}
+    	MultiP MUX1 = new MultiP("MUX1","1029X32",ReadRegisterNumberA[0],ReadRegisterNumberA[1],ReadRegisterNumberA[2],ReadRegisterNumberA[3],ReadRegisterNumberA[4]);
+    	
+    	MultiP MUX2 = new MultiP("MUX2","1029X32",ReadRegisterNumberB[0],ReadRegisterNumberB[1],ReadRegisterNumberB[2],ReadRegisterNumberB[3],ReadRegisterNumberB[4]);
+    	for(int i=0; i<32;++i) {
+    		for(int j=0; j<32;++j) {
+    			MUX1.addInput(Reg[i].getOutput(j));
+    		}
+    	}
+    	for(int i=0; i<32; ++i) {
+    		for(int j=0; j<32; ++j) {
+    			MUX2.addInput(Reg[i].getOutput(j));
+    		}
+    	}
+    	
 
         Simulator.debugger.addTrackItem(clk,mp);
         Simulator.debugger.setDelay(500);
