3cda1a55794c61d2c897042295c021da5518e53c
ehssanmp <59968835+ehssanmp@users.noreply.github.com>
2020-07-19 19:54:29 +0430

    Add files via upload

--- AluControl.java
+++ AluControl.java
@@ -18,21 +18,28 @@ public class AluControl extends Wrapper {
 		// TODO Auto-generated method stub
 		And and1= new And("A1");
 		And and2= new And("A2");
+		And and3= new And("A3");
 		Or or1= new Or("O1");
 		Or or2= new Or("O2");
 		Or or3 = new Or("O3");
 		Not not1= new Not("N1");
 		Not not2= new Not("N2");
-		and1.addInput(getInput(1),getInput(3));
-		or1.addInput(and1.getOutput(0),getInput(0));
-		not1.addInput(getInput(1));
-		not2.addInput(getInput(4));
-		or2.addInput(not1.getOutput(0),not2.getOutput(0));
+		Not not3= new Not("N3");
+		not1.addInput(getInput(0));
+		and1.addInput(not1.getOutput(0),getInput(0));//Operation3
+		and2.addInput(getInput(1),getInput(3));
+		or1.addInput(getInput(0),and2.getOutput(0));//Operation2
+		not2.addInput(getInput(1));
+		not3.addInput(getInput(4));
+		or2.addInput(not2.getOutput(0),not3.getOutput(0));//Operation1
 		or3.addInput(getInput(2),getInput(5));
-		and2.addInput(getInput(1),or3.getOutput(0));
-		addOutput(and2.getOutput(0));
+		and3.addInput(getInput(1),or3.getOutput(0));//Operation0
+		addOutput(and3.getOutput(0));
 		addOutput(or2.getOutput(0));
 		addOutput(or1.getOutput(0));
+		addOutput(and1.getOutput(0));
+		
+		
 	}
 	
 }
